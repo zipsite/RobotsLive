3213bce8bc245c2389641209a49ec2baaddd00e6
BuildTools <miassilya5@gmail.com>
2022-04-24 22:15:47 +0300

    Добавление закрытия меню при нажатии

--- access/js/nav-menu.js
+++ access/js/nav-menu.js
@@ -2,10 +2,19 @@ var btn = document.getElementById("nav-btn");
 var nav = document.getElementById("nav-menu");
 
 btn.onclick = function () {
-    if(nav.getAttribute("class") == "nav nav-close transition") {
+    if (nav.getAttribute("class") == "nav nav-close transition") {
         nav.setAttribute("class", "nav transition");
     }
     else {
         nav.setAttribute("class", "nav nav-close transition");
     }
 };
+
+var links = document.getElementsByClassName("close-nav");
+
+for (var i = 0; i < links.length; i++) {
+
+    links[i].addEventListener("click", function (e) {
+        nav.setAttribute("class", "nav nav-close transition");
+    });
+}
