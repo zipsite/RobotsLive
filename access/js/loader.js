102a6de7d0a5917d053106c18f10092c7834fd8f
web_cino <miassilya5@gmail.com>
2022-04-24 21:44:24 +0300

    Настройка шаблонизатора и переделка под spa

---
+++ access/js/loader.js
@@ -0,0 +1,54 @@
+
+
+OnLoad();
+
+function OnLoad()
+{
+    var link = window.location.pathname; //Ссылка страницы без домена
+
+    LinkClick(link);
+}
+
+
+
+function LinkClick(href)
+{
+    var props = href.split("/"); 
+    //Получаем параметры из ссылки. 1 - раздел, 2 - идентификатор
+    var Handler = function(Request)
+    {
+        document.getElementById("content").innerHTML = Request.responseText;
+        InitLinks();
+    }
+    
+    if (props[1] == '') {
+        SendRequest("GET", "/views/home.html", "", Handler);
+    }
+    else if (props[1] == 'catalog') {
+        SendRequest("GET", "/views/catalog.html", "", Handler);
+    }
+    else if (props[1] == 'staff') {
+        SendRequest("GET", "/views/staff.html", "", Handler);
+    }
+    else if (props[1] == 'basket') {
+        SendRequest("GET", "/views/basket.html", "", Handler);
+    }
+    
+}
+
+function InitLinks()
+{
+    var links = document.getElementsByClassName("spec-a"); //Находим все ссылки на странице
+
+    for (var i = 0; i < links.length; i++)
+    {
+   	 //Отключаем событие по умолчанию и вызываем функцию LinkClick
+   	 links[i].addEventListener("click", function (e)
+   	 {
+   		 e.preventDefault();
+         console.log(e.currentTarget.getAttribute("href"));
+   		 LinkClick(e.currentTarget.getAttribute("href"));  
+   		 return false;
+   	 });
+    }
+}
