55bbe2cec09e7f6a0f8138c1313a8fad75309f5c
BuildTools <miassilya5@gmail.com>
2022-04-27 19:31:15 +0300

    Изменение темы браузера

--- access/js/toggle-theme.js
+++ access/js/toggle-theme.js
@@ -4,4 +4,10 @@ var bodybox = document.getElementById("bodybox");
 btnToggle.onclick = function () {
     bodybox.classList.toggle("day-theme");
     bodybox.classList.toggle("night-theme");
+    if (document.querySelector('meta[name="theme-color"]').getAttribute("content") == "#000000"){
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#ffffff");
+    }
+    else {
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#000000");
+    }
 };
