cc67223b72efa31cbb3fd5d79d9db9f13c305406
BuildTools <miassilya5@gmail.com>
2022-04-27 20:05:03 +0300

    save theme parametr

--- access/js/toggle-theme.js
+++ access/js/toggle-theme.js
@@ -1,13 +1,26 @@
 var btnToggle = document.getElementById("toggle-theme");
 var bodybox = document.getElementById("bodybox");
 
-btnToggle.onclick = function () {
+var themeStatus = localStorage.getItem('themeapp');
+if (themeStatus == 'day-theme' || themeStatus == 'night-theme') {
+    if(themeStatus == 'day-theme') {
+        bodybox.classList.toggle("day-theme");
+        bodybox.classList.toggle("night-theme");
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#ffffff");
+    }
+}
+
+function toggleTheme() {
     bodybox.classList.toggle("day-theme");
     bodybox.classList.toggle("night-theme");
     if (document.querySelector('meta[name="theme-color"]').getAttribute("content") == "#000000"){
         document.querySelector('meta[name="theme-color"]').setAttribute("content", "#ffffff");
+        localStorage.setItem('themeapp', 'day-theme');
     }
     else {
         document.querySelector('meta[name="theme-color"]').setAttribute("content", "#000000");
+        localStorage.setItem('themeapp', 'night-theme');
     }
-};
+}
+
+btnToggle.onclick = toggleTheme;
